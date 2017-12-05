55df69e2fdb6043c1a6c494b7a3b04f0cba05e7e
Ian E McAllister <ianmcallistersubscriptions@gmail.com>
2017-12-04 16:47:48 -0800

    Express Server

--- index.js
+++ index.js
@@ -4,3 +4,27 @@
 *
 */
 
+//declare dependencies
+var express = require('express');
+
+//return the express object
+var app = express();
+
+//environment variables
+var port = process.env.PORT || 3000;
+
+//get the URL encoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+var jsonParser = bodyParser.json();
+
+//tell it the folder to serve
+app.use(express.static('dist'));
+
+//define our body parsers
+app.use(jsonParser); // for parsing application/json
+app.use(urlencodedParser); // for parsing application/x-www-form-urlencoded
+
+//open the port for local development
+app.listen(port,function() {
+	console.log('Express server is up and running on port ' + port);
+});
